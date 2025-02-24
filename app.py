e99b578bccde7767dbbb1217e6414add0c42b034
Lorenzo Anastasi <lorenzo@HP>
2025-02-24 14:26:33 +0100

    Add student model

--- app.py
+++ app.py
@@ -4,12 +4,39 @@ from flask_sqlalchemy import SQLAlchemy
 
 from sqlalchemy.sql import func
 
+# store in a variable the directory of app.py
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# define the path to database and the configuration options
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# create an instance to database
 db = SQLAlchemy(app)
+
+# define the model for the table Student
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    bio = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Student {self.firstname}>'
+    
+# create a route for displaing all students
+@app.route('/')
+def index():
+    students = Student.query.all()
+    return render_template('index.html', students=students)
+
+@app.route('/int:<student_id>/')
+def student(student_id):
+    student = Student.query.get_or_404(student_id)
+    return render_template('student.html', student=student)
 
