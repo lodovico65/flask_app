a227f7a9cfb76def7bc8c39afc10b57049c7df9f
Lorenzo Anastasi <lorenzo@HP>
2025-02-24 16:53:27 +0100

    CRUD

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # define the path to database and the configuration options
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # create an instance to database
@@ -39,4 +40,56 @@ def index():
 def student(student_id):
     student = Student.query.get_or_404(student_id)
     return render_template('student.html', student=student)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        age = int(request.form['age'])
+        bio = request.form['bio']
+        student = Student(firstname=firstname,
+                          lastname=lastname,
+                          email=email,
+                          age=age,
+                          bio=bio)
+        db.session.add(student)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    
+    return render_template('create.html')
+
+@app.route('/<int:student_id>/edit/', methods=['GET', 'POST'])
+def edit(student_id):
+    student = Student.query.get_or_404(student_id)
+
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        age = int(request.form['age'])
+        bio = request.form['bio']
+
+        student.firstname = firstname
+        student.lastname = lastname
+        student.email = email
+        student.age = age
+        student.bio = bio
+
+        db.session.add(student)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', student=student)
+
+@app.post('/<int:student_id>/delete/')
+def delete(student_id):
+    student = Student.query.get_or_404(student_id)
+    db.session.delete(student)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
